f288c697adc6a2a192f88d2aef7b7168e9f86412
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-02-18 09:36:30 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (26.70%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,3 +1,6 @@
+
+
+// 1st method without if loop
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
@@ -7,35 +10,78 @@ public:
         int n = matrix[0].size();
     int minr =0 , minc = 0;
     int maxr = m-1, maxc = n-1;
+    int tne = m*n;
+    int count =0;
         vector<int> v;
     while(minr<=maxr && minc<=maxc){
         // right
-        for(int j=minc;j<=maxc;j++){
+        for(int j=minc;j<=maxc && count<tne;j++){
             v.push_back(matrix[minr][j]);
+            count++;
         }
         minr++;
-        if(minr>maxr || minc>maxc) break;
         // down
-        for(int i=minr;i<=maxr;i++){
+        for(int i=minr;i<=maxr &&count<tne;i++){
         v.push_back(matrix[i][maxc]);
+         count++;
         }
         maxc--;
-        if(minr>maxr || minc>maxc) break;
         // left
-        for(int j=maxc;j>=minc;j--){
-                        v.push_back(matrix[maxr][j]);
+        for(int j=maxc;j>=minc&& count<tne;j--){
+         v.push_back(matrix[maxr][j]);
+          count++;
         }
         maxr--;
-        if(minr>maxr || minc>maxc) break;
         // up
-        for(int i=maxr;i>=minr;i--){
-                        v.push_back(matrix[i][minc]);
+        for(int i=maxr;i>=minr && count<tne;i--){
+            v.push_back(matrix[i][minc]);
+             count++;
         }
         minc++;
-        if(minr>maxr || minc>maxc) break;
          }
       return v;                              
-     }
-    
+     }  
     
 };
+
+// class Solution {
+// public:
+//     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+//     // spiral
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+//     int minr =0 , minc = 0;
+//     int maxr = m-1, maxc = n-1;
+//         vector<int> v;
+//     while(minr<=maxr && minc<=maxc){
+//         // right
+//         for(int j=minc;j<=maxc;j++){
+//             v.push_back(matrix[minr][j]);
+//         }
+//         minr++;
+//         if(minr>maxr || minc>maxc) break;
+//         // down
+//         for(int i=minr;i<=maxr;i++){
+//         v.push_back(matrix[i][maxc]);
+//         }
+//         maxc--;
+//         if(minr>maxr || minc>maxc) break;
+//         // left
+//         for(int j=maxc;j>=minc;j--){
+//                         v.push_back(matrix[maxr][j]);
+//         }
+//         maxr--;
+//         if(minr>maxr || minc>maxc) break;
+//         // up
+//         for(int i=maxr;i>=minr;i--){
+//                         v.push_back(matrix[i][minc]);
+//         }
+//         minc++;
+//         if(minr>maxr || minc>maxc) break;
+//          }
+//       return v;                              
+//      }
+    
+    
+// };
