7d35c2c1513b389e2ba9d2cf11f992feb88ea491
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-03-27 15:53:02 +0530

    Time: 11 ms (91.09%), Space: 21.6 MB (38.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int> v;
-        int n = nums.size();
-        int noz = 0;
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0) v.push_back(nums[i]);
-            else noz++;
-        }
-        for(int i=1;i<=noz;i++) v.push_back(0);
-        
-        for(int i=0;i<n;i++){
-        nums[i] = v[i];
-        }
-        
+       int n = nums.size();
+        int i =0; // i for travel zero
+        int j= 0; // j for travel non-zero
+        while(i<n && j<n){
+            if(nums[i] == 0 && nums[j] != 0){
+                if(i<j) swap(nums[i],nums[j]);
+                else j++;
+              }
+            
+            if(nums[i] != 0) i++;
+            if(nums[j] == 0) j++;
+            }
     }
 };
+
+// 2nd method using extra space
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         vector<int> v;
+//         int n = nums.size();
+//         int noz = 0;
+//         for(int i=0;i<n;i++){
+//             if(nums[i]!=0) v.push_back(nums[i]);
+//             else noz++;
+//         }
+//         for(int i=1;i<=noz;i++) v.push_back(0);
+        
+//         for(int i=0;i<n;i++){
+//         nums[i] = v[i];
+//         }
+        
+//     }
+// };
