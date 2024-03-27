e6aa017462186008f3d9a1d4d30bcb13e28b728c
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-03-27 15:54:09 +0530

    Time: 16 ms (53.20%), Space: 21.7 MB (38.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -15,23 +15,26 @@ public:
             }
     }
 };
+/*
 
 // 2nd method using extra space
-// class Solution {
-// public:
-//     void moveZeroes(vector<int>& nums) {
-//         vector<int> v;
-//         int n = nums.size();
-//         int noz = 0;
-//         for(int i=0;i<n;i++){
-//             if(nums[i]!=0) v.push_back(nums[i]);
-//             else noz++;
-//         }
-//         for(int i=1;i<=noz;i++) v.push_back(0);
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> v;
+        int n = nums.size();
+        int noz = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0) v.push_back(nums[i]);
+            else noz++;
+        }
+        for(int i=1;i<=noz;i++) v.push_back(0);
         
-//         for(int i=0;i<n;i++){
-//         nums[i] = v[i];
-//         }
+        for(int i=0;i<n;i++){
+        nums[i] = v[i];
+        }
         
-//     }
-// };
+    }
+};
+
+*/
