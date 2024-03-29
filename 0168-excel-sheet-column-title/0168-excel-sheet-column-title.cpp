640eb57db7d960f2a3d331bb13d4b9fee44fd83b
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-03-29 21:23:52 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (68.05%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string ans = "";
+        while(n>0){
+        n--;
+        int curr = n % 26;
+            n = n/26;
+            ans.push_back((char)(curr + 'A'));
+            
+            }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
