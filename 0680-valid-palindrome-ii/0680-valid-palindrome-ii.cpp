7a861777bd46b38d0d969fe0512438d015eba14d
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-04-05 23:25:21 +0530

    Time: 58 ms (36.30%), Space: 25.3 MB (64.27%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -13,6 +13,31 @@ public:
         int i=0;
         int j =n-1;
         while(i<j){
+            if(s[i]!=s[j]){ 
+                return isPalindrome(s,i,j-1) || isPalindrome(s,i+1,j);
+            }
+             i++;
+             j--;
+        }
+        return true;
+    }
+};
+/*
+class Solution {
+public:
+    bool isPalindrome(string s , int i, int j){
+        while(i<j){
+            if(s[i]!=s[j])   return false;
+                i++;
+                j--;
+        }   
+    return true;
+    }
+    bool validPalindrome(string s) {
+        int n = s.length();
+        int i=0;
+        int j =n-1;
+        while(i<j){
             if(s[i]==s[j]){
                 i++;
                 j--;
@@ -24,3 +49,4 @@ public:
         return true;
     }
 };
+*/
