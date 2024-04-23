1ddde405203d53c9a7141aefc2b8dd3107c9d157
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-04-23 12:45:26 +0530

    Time: 2 ms (56.77%), Space: 7 MB (89.65%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        // int x = num;
-        //   int lo = 0;
-        //  int hi = x;
-        // while(lo<=hi){
-        //     int mid = lo + (hi-lo)/2;
-        //     long long m = (long long)mid;
-        //     long long y = (long long)x;
-        //     if((m*m)==y) return true;
-        //     else if((m*m)>y) hi = mid-1;
-        //     else lo = mid+1;
-        // }
-        // return false;
-        int root = sqrt(num);
-        if(root*root==num) return true;
-        else return false;
+        int x = num;
+          int lo = 0;
+         int hi = x;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            long long m = (long long)mid;
+            long long y = (long long)x;
+            if((m*m)==y) return true;
+            else if((m*m)>y) hi = mid-1;
+            else lo = mid+1;
+        }
+        return false;
+        // int root = sqrt(num);
+        // if(root*root==num) return true;
+        // else return false;
     }
 };
