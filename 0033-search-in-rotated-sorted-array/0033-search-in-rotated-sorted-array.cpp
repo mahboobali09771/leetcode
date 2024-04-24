f297d570a7121a98c7d0cf48a72435de2b358018
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-04-24 22:47:58 +0530

    Time: 4 ms (41.60%), Space: 13.6 MB (35.38%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,6 +2,32 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
+        int low = 0, high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]<=nums[high]){
+                if(target>=nums[mid] && target<=nums[high])
+                    low = mid + 1;
+                else high = mid - 1;
+            }
+            else{
+                if(target>=nums[low] && target<=nums[mid])
+                    high = mid - 1;
+                else low= mid + 1;
+            }
+        }
+        return -1; 
+    }
+};
+
+
+// 2nd method
+/*
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
         int low = 0;
         int high = n-1;
         int pivot = -1;
@@ -62,25 +88,6 @@ public:
             }
         }
         return -1; 
-        
-        
-        // // 2nd method
-        // int n = nums.size();
-        // int low = 0, high = n-1;
-        // while(low<=high){
-        //     int mid = low + (high-low)/2;
-        //     if(nums[mid]==target) return mid;
-        //     else if(nums[mid]<=nums[high]){
-        //         if(target>=nums[mid] && target<=nums[high])
-        //             low = mid + 1;
-        //         else high = mid - 1;
-        //     }
-        //     else{
-        //         if(target>=nums[low] && target<=nums[mid])
-        //             high = mid - 1;
-        //         else low= mid + 1;
-        //     }
-        // }
-        // return -1;
     }
 };
+*/
