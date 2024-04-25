45a60db8b3277b8c31d80d8de36e36612431b49e
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-04-25 23:35:59 +0530

    Time: 32 ms (57.01%), Space: 21.2 MB (91.00%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool check(int speed,vector<int>& piles, int h){
+        long long count = 0;
+        int n = piles.size();
+        for(int i=0;i<n;i++){
+            // if(count>h) return false;
+            if(speed>=piles[i]) count++;
+            else if(piles[i]%speed==0) count += (long long) (piles[i]/speed);
+            else count += (long long )(piles[i]/speed + 1); 
+        }
+        if(count>(long long)h) return false;
+        else return true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int mx = -1;
+        for(int i=0;i<n;i++) {
+            mx = max(mx,piles[i]);
+            
+        }
+        int lo = 1;
+        int hi = mx;
+        int ans = -1;
+        while(lo<=hi) {
+            int mid = lo + (hi - lo)/2;
+            if(check(mid,piles,h)==true){
+                ans = mid;
+                hi = mid-1;  
+            }
+            else lo = mid + 1;
+        }
+        return ans;
+    }
+};
