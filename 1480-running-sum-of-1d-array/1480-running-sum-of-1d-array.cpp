6923a9201fb538b1485b8c94c642c878080096e8
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-05-25 23:03:39 +0530

    Time: 0 ms (100.00%), Space: 10.6 MB (54.15%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -2,6 +2,19 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         int n = nums.size();
+        for(int i=1;i<n;i++){
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+};
+// 2nd method TC O(n)  SC O(n)
+
+/*
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
         vector<int> run(n);
         run[0] = nums[0];
         for(int i=1;i<n;i++){
@@ -10,3 +23,4 @@ public:
         return run;
     }
 };
+*/
