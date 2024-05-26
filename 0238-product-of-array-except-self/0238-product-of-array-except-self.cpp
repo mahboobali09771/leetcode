f5f304bae849754e23be7ffce5275d19d934faa9
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-05-26 07:38:56 +0530

    Time: 29 ms (45.80%), Space: 38.1 MB (87.68%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,6 +3,32 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
         vector<int> pre(n);
+        // prefix product array
+        int p = nums[0];
+        pre[0] = 1;
+        for(int i=1;i<n;i++){
+            pre[i] = p;
+            p *= nums[i];
+        }
+        // suffix product array (in prefix only)
+        p = nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            pre[i] *= p;
+            p *= nums[i];
+        }
+        return pre;
+  
+    }
+};
+
+
+// reduce vector
+/*
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> pre(n);
         vector<int> suf(n);
         // prefix product array
         int p = nums[0];
@@ -26,7 +52,7 @@ public:
   
     }
 };
-
+*/
 // wrong method 
 /*
 class Solution {
