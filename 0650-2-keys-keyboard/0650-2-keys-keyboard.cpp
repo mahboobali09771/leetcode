308303923ce72df864e891feab8553b9b8133ab9
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-05-30 21:11:22 +0530

    Time: 3 ms (73.90%), Space: 7.1 MB (72.36%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,5 +1,38 @@
 class Solution {
 public:
+    bool isPrime(int n){
+        if(n==1) return false;
+        for(int i=2;i<=sqrt(n);i++){
+            if(n%i==0) return false;
+        }
+        return false;
+        
+    }
+    int gd(int n){
+        for(int i=2;i<=sqrt(n);i++){
+            if(n%i==0) return n/i;
+        }
+        return 1;
+    }
+    int minSteps(int n) {
+       int count = 0;
+        while(n>1){ // O(log(n))
+            if(isPrime(n)){
+                count += n;
+                break;
+            }
+            int hf = gd(n); // O(sqrt(n))
+            count += (n/hf);
+            n = hf;
+        }
+        return count;
+    }
+};
+
+// 2nd method
+/*
+class Solution {
+public:
     int gd(int n){
         for(int i=n/2;i>=1;i--){
             if(n%i==0) return i;
@@ -16,3 +49,4 @@ public:
         return count;
     }
 };
+*/
