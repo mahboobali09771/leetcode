3ca7ffa6d60271207697067674f94b0803155dc7
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-06-25 17:52:57 +0530

    Time: 363 ms (68.63%), Space: 183.9 MB (19.07%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -8,25 +8,41 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+ class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
         ListNode* temp = head;
-        int len = 0;
-        while(temp!=NULL){
-            len++;
-            temp = temp->next;
+        while(--k) temp = temp->next;
+        ListNode* p1 = temp->next;
+        ListNode* p2 = head;
+        while(p1){
+            p1 = p1->next;
+            p2 = p2->next;
         }
-        ListNode* first = head;
-        ListNode* second = head;
-        int m = len - k + 1;
-        for(int i=1;i<k;i++){
-            first = first->next;
-        }
-        for(int i=1;i<m;i++){
-            second = second->next;
-        }
-        swap(first->val,second->val);
+        swap(temp->val,p2->val);
         return head;
     }
 };
+
+// class Solution {
+// public:
+//     ListNode* swapNodes(ListNode* head, int k) {
+//         ListNode* temp = head;
+//         int len = 0;
+//         while(temp!=NULL){
+//             len++;
+//             temp = temp->next;
+//         }
+//         ListNode* first = head;
+//         ListNode* second = head;
+//         int m = len - k + 1;
+//         for(int i=1;i<k;i++){
+//             first = first->next;
+//         }
+//         for(int i=1;i<m;i++){
+//             second = second->next;
+//         }
+//         swap(first->val,second->val);
+//         return head;
+//     }
+// };
