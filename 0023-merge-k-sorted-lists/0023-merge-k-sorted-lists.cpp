c5b9448c25834c1ed9ec32d9cb1065a7328288c4
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-07-03 13:16:54 +0530

    Time: 31 ms (26.79%), Space: 17.1 MB (40.64%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -33,6 +33,44 @@ public:
     ListNode* mergeKLists(vector<ListNode*>& arr) {
         if(arr.size()==0) return NULL;
         while(arr.size()>1){
+            ListNode* a = arr[0];
+            arr.erase(arr.begin());  // remove from starting or pop from starting
+            ListNode* b = arr[0];
+            arr.erase(arr.begin());  // remove from starting or pop from starting
+            ListNode* c = mergeTwoLists(a,b);
+            arr.push_back(c);      
+        }
+        return arr[0];
+    }
+};
+
+// 2nd method
+/*
+class Solution {
+public:
+        ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        // SC = O(1)
+        ListNode* c = new ListNode(100);
+        ListNode* temp = c;
+        while(a!=NULL && b!=NULL){
+            if(a->val <= b->val){
+                temp->next = a;
+                a =  a->next;
+                temp = temp->next;
+            }
+            else{ // (a->val > b->val)
+               temp->next = b;
+                b =  b->next;
+                temp = temp->next;
+            }
+        }
+        if(a==NULL) temp->next = b;
+        else temp->next = a;
+        return c->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& arr) {
+        if(arr.size()==0) return NULL;
+        while(arr.size()>1){
             ListNode* a = arr[arr.size()-1];
             arr.pop_back();
             ListNode* b = arr[arr.size()-1];
@@ -43,3 +81,4 @@ public:
         return arr[0];
     }
 };
+*/
