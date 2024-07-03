fa8712cc167f7ec77360d82dd1870653fbf08057
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-07-03 10:21:27 +0530

    Time: 3 ms (81.96%), Space: 18.2 MB (75.80%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,6 +11,33 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        // SC = O(1)
+        ListNode* c = new ListNode(100);
+        ListNode* temp = c;
+        while(a!=NULL && b!=NULL){
+            if(a->val <= b->val){
+                temp->next = a;
+                a =  a->next;
+                temp = temp->next;
+            }
+            else{ // (a->val > b->val)
+               temp->next = b;
+                b =  b->next;
+                temp = temp->next;
+            }
+        }
+        if(a==NULL) temp->next = b;
+        else temp->next = a;
+        return c->next;
+    }
+};
+
+// 2nd method
+/*
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* a, ListNode* b) {
+        // SC = O(m+n)
         ListNode* tempA = a;
         ListNode* tempB = b;
         ListNode* c = new ListNode(100);
@@ -38,3 +65,4 @@ public:
         return c->next;
     }
 };
+*/
