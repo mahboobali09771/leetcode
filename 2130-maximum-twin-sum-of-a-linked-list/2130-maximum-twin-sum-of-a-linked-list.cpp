7c0c411c6cb4fda49b20abab3cba8c661d3b4052
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-07-16 10:49:31 +0530

    Time: 190 ms (32.27%), Space: 119.3 MB (55.72%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -23,6 +23,40 @@ public:
 }
     int pairSum(ListNode* head) {
         ListNode* temp = head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* r = reverseList(slow->next);
+        int sum = 0;
+        while(r){
+             if(sum<(temp->val+r->val))  sum = temp->val+r->val;
+             temp = temp->next;
+             r = r->next;
+        }
+       return sum;
+    }
+};
+
+// method 2
+/* 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL ) return head;
+       ListNode* prev = NULL, *curr = head, *Next = head;
+        while(curr!=NULL){
+            Next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = Next;
+        }
+        return prev;
+}
+    int pairSum(ListNode* head) {
+        ListNode* temp = head;
         int n = 0;
         while(temp){
             temp = temp->next;
@@ -45,3 +79,4 @@ public:
        return sum;
     }
 };
+*/
