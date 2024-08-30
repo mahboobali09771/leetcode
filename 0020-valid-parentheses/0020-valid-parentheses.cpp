563c3779191c339ca4901dccfa3bc8c79725261c
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-08-30 08:26:09 +0530

    Time: 3 ms (40.55%), Space: 7.8 MB (52.71%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length()%2!=0) return false;
+        stack<char> st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+                st.push(s[i]);
+            }
+            else if(s[i]==')'){
+               if(st.size()==0) return false;
+                 else if(st.top()=='(') st.pop();
+                else return false;
+            }
+            else if(s[i]==']'){
+                if(st.size()==0) return false;
+                else if(st.top()=='[') st.pop();
+                else return false;
+            }
+             else if(s[i]=='}'){
+                if(st.size()==0) return false;
+                else if(st.top()=='{') st.pop();
+                 else return false;
+            }
+            // else{ // s[i]==')' || s[i]==']' || s[i]=='}'
+            //     if(st.size()==0) return false;
+            //     else st.pop();
+            // }
+        }
+        if(st.size()==0) return true;
+        else return false;
+        
+    }
+};
