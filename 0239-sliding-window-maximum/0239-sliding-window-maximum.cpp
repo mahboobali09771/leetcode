64925602d2d62c3ae3e4eb066a2a128d1250ea07
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-09-01 21:51:22 +0530

    Time: 181 ms (38.42%), Space: 137.4 MB (35.24%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,4 +1,24 @@
+//   using dequeue
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
+        if(k==1) return arr;
+        int n = arr.size();
+        deque<int>dq; // decreasing order of element (index)
+        vector<int> ans; // i should be min k-1
+        for(int i=0;i<n;i++){
+            while(dq.size()>0 && arr[i]>arr[dq.back()]) dq.pop_back();
+            dq.push_back(i);
+            int j = i-k+1; // start of the window
+            while(dq.front()<j) dq.pop_front();
+            if(i>=k-1) ans.push_back(arr[dq.front()]);
+        }
+        return ans;
+    }
+};
+
 //   O(n)
+/*
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& arr, int k) {
@@ -33,23 +53,7 @@ public:
         return ans;
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 // brute force solution  O(n*k)
 /*
 class Solution {
