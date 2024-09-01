db956700965a424069bf0f9fef8e7c8b200e6967
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2024-09-01 18:19:44 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (19.83%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,3 +1,56 @@
+// m1 pop efficient
+class MyQueue {
+public:
+    stack<int>st;
+    stack<int>helper;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) { // O(n)
+        if(st.size()==0){
+            st.push(x);
+            return;
+        } 
+        else {
+            while(st.size()>0){
+                helper.push(st.top());
+                st.pop();
+            }
+            st.push(x);
+        while(helper.size()>0){
+            st.push(helper.top());
+            helper.pop();
+        }
+            
+        }
+    }
+     
+    int pop() { // O(1)
+            int x = st.top();
+            st.pop();
+        return x;
+    }
+    
+    int peek() { // front // O(1)
+        return st.top();
+    }
+    
+    bool empty() {
+        if(st.size()==0) return true;
+        else return false;
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+/*  // m2 push efficient
 class MyQueue {
 public:
     stack<int>st;
@@ -46,6 +99,7 @@ public:
         else return false;
     }
 };
+*/
 
 /**
  * Your MyQueue object will be instantiated and called as such:
