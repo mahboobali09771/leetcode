43408d78a256bc4464e775700f8958d05bb315d4
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-03-18 11:35:06 +0530

    Time: 0 ms (100%), Space: 16.6 MB (98.78%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -11,6 +11,32 @@
  */
 class Solution {
 public:
+    int levels(TreeNode* root){
+            if(root==NULL) return 0;
+            return 1 + max(levels(root->left),levels(root->right));
+        }
+   
+    void lOrder(TreeNode* root,vector<vector<int>>& ans,int level){
+        if(root==NULL) return;
+        ans[level].push_back(root->val);
+        lOrder(root->left,ans,level+1);
+        lOrder(root->right,ans,level+1);
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        int n = levels(root);
+        vector<vector<int>> ans;
+        for(int i=1;i<=n;i++){
+            vector<int> v;
+            ans.push_back(v);
+        }
+        lOrder(root,ans,0);
+        return ans;
+    }
+};
+
+ /*
+class Solution {
+public:
     int level(TreeNode* root){
             if(root==NULL) return 0;
             return 1 + max(level(root->left),level(root->right));
@@ -39,3 +65,4 @@ public:
         return ans;
     }
 };
+*/
