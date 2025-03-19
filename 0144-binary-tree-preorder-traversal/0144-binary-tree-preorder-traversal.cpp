19b26490be49103b748cfeee294e970150f214cd
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-03-19 11:39:52 +0530

    Time: 0 ms (100%), Space: 10.8 MB (72.33%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -9,6 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ // iterative
+ class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        if(root!=NULL) st.push(root);
+        while(st.size()>0){
+            TreeNode* temp = st.top();
+            st.pop();
+            ans.push_back(temp->val);
+            if(temp->right!=NULL) st.push(temp->right);
+            if(temp->left!=NULL) st.push(temp->left);
+        }
+        return ans;
+    }
+};
+// recursive
+ /*
 class Solution {
 public:
     void preorder(TreeNode* root, vector<int>& ans){
@@ -23,3 +42,4 @@ public:
         return ans;
     }
 };
+*/
