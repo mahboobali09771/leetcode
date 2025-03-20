a780d8b30565fc61bec57719a6cbd3ce45e63811
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-03-20 09:05:08 +0530

    Time: 0 ms (100%), Space: 14.9 MB (64.19%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -9,6 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ class Solution {
+public:
+        int levels(TreeNode* root){
+            if(root==NULL) return 0;
+            return 1 + max(levels(root->left),levels(root->right));
+        }
+        void preOrder(TreeNode* root,vector<int>& ans, int level){
+            if(root==NULL) return;
+            ans[level] = root->val;
+            preOrder(root->left,ans,level+1);
+            preOrder(root->right,ans,level+1);
+        }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans(levels(root),0);
+        preOrder(root,ans,0);
+
+       return ans; 
+    }
+    
+};
+ /*
 class Solution {
 public:
         int level(TreeNode* root){
@@ -38,3 +59,4 @@ public:
     }
     
 };
+*/
