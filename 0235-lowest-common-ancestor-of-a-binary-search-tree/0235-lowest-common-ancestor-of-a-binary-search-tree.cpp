83c34503e275ab5e2a1185d9061e64d0849e1432
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-03-27 14:55:15 +0530

    Time: 24 ms (44.95%), Space: 23.3 MB (90.14%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -7,7 +7,15 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right,p,q);
+        else if(root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left,p,q);
+        return root;
+    }
+};
+/*
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -18,3 +26,4 @@ public:
         else return lowestCommonAncestor(root->right,p,q);
     }
 };
+*/
