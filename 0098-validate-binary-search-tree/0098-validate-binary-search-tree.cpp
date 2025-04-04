ada47750fc2e12edab61ee4780ae840a4dff2aac
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-04-04 09:07:01 +0530

    Time: 0 ms (100%), Space: 22.2 MB (10%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,6 +9,24 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ class Solution {
+public:
+    void inorder(TreeNode* root,vector<int>& ans){
+        if(root==NULL) return;
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+    bool isValidBST(TreeNode* root) {
+        vector<int> ans;
+        inorder(root, ans);
+        for(int i=1;i<ans.size();i++){
+            if(ans[i]<=ans[i-1]) return false;
+        }
+        return true;
+    }
+};
+ /*
 class Solution {
 public:
     long long maxTree(TreeNode* root){
@@ -26,3 +44,4 @@ public:
         return isValidBST(root->left) && isValidBST(root->right);
     }
 };
+*/
