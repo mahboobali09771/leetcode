b1a6a6b5bee4af17e4efd24cf71d13b0b882010c
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-05-02 09:19:24 +0530

    Time: 0 ms (100%), Space: 10.8 MB (88.1%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,6 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+  class Solution {
+public:
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        TreeNode* curr = root;
+        while(curr!=NULL){
+            if(curr->left!=NULL){ // find the pred
+                TreeNode* pred = curr->left;
+                while(pred->right!=NULL && pred->right!=curr){
+                    pred = pred->right;
+                }
+                if(pred->right==NULL){ // Link
+                    pred->right = curr;
+                    curr = curr->left;
+                }
+                else{ // pred->right == curr : unlink
+                    pred->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+
+                }
+            }
+            else{ // curr->left == NULL
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+        }
+        return ans;
+    }
+};
+/*
  class Solution {
 public:
 
@@ -31,6 +63,7 @@ public:
         return ans;
     }
 };
+*/
  /*
 class Solution {
 public:
