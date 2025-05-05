0460428b4e5010c6625fbdf7056f221f4891a946
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-05-05 08:42:12 +0530

    Time: 0 ms (100%), Space: 17.4 MB (77.19%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -9,6 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ // M1
+ class Solution {
+public:
+
+    void flatten(TreeNode* root) {
+        if(root==NULL) return;
+        TreeNode* l = root->left;
+        TreeNode* r = root->right;
+        root->left = NULL;
+        root->right = NULL;
+        flatten(l);
+        flatten(r);
+        root->right = l;
+        TreeNode* temp = root;
+        while(temp->right!=NULL) temp = temp->right;
+        temp->right = r;
+     
+    }
+}; 
+// M2   
+/*
 class Solution {
 public:
     vector<TreeNode*> preorderTraversal(TreeNode* root){
@@ -33,3 +54,4 @@ public:
         }
     }
 };
+*/
