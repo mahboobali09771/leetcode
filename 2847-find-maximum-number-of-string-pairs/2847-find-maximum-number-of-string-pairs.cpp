e664278758383960923a4b26a5c18313e340050f
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-05-11 05:53:53 +0530

    Time: 3 ms (54.26%), Space: 25.6 MB (62.39%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
@@ -3,6 +3,22 @@ public:
     int maximumNumberOfStringPairs(vector<string>& arr) {
         int n = arr.size();
         int count = 0;
+        unordered_set<string> s;
+        for(int i=0;i<n;i++){
+            string rev = arr[i];
+            reverse(rev.begin(),rev.end());
+            if(s.find(rev)!=s.end()) count++;
+            else s.insert(arr[i]);
+        }
+        return count;
+    }
+};
+/*
+class Solution {
+public:
+    int maximumNumberOfStringPairs(vector<string>& arr) {
+        int n = arr.size();
+        int count = 0;
         for(int i=0;i<n-1;i++){
             string rev = arr[i];
             reverse(rev.begin(),rev.end());
@@ -13,3 +29,4 @@ public:
         return count;
     }
 };
+*/
