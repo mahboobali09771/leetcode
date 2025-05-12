35c9eb81c5a14df6c169b5c20e7c910e536ec1a0
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-05-12 06:04:49 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.7%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,4 +1,30 @@
-class Solution {
+ class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        unordered_map<char,int> map1; // for s
+        unordered_map<char,int> map2; // for t
+        for(int i=0;i<s.length();i++){
+            map1[s[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            map2[t[i]]++;
+        }
+        for(auto x : map1){
+            char ch1 = x.first;
+            int freq1 = x.second;
+            if(map2.find(ch1)!=map2.end()){
+                int freq2 = map2[ch1];
+                if(freq1!=freq2) return false;
+            }
+            else return false;
+        }
+        return true;
+    }
+};
+
+// m3
+/* class Solution {
 public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
@@ -7,3 +33,4 @@ public:
         else return false;
     }
 };
+*/
