7c62ad4729e2113aa47c0f7df0e6b45cfcb904da
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-06-29 17:45:32 +0530

    Time: 107 ms (38.05%), Space: 75.8 MB (27.34%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        int ans = 0;
-        for(auto &i : nums){
-            if(mp[k-i]>0){
-                mp[k-i]--;
-                ans++;
-            } 
-            else mp[i]++;
-
+        unordered_map<int,int>mp;
+        int count = 0;
+        for(auto x:nums) mp[x]++;
+        for(auto x:nums){
+            int diff = k - x;
+            if(diff==x){
+                count += (mp[diff]/2);
+                mp.erase(x);
+            }
+            else if(mp.count(diff)){
+                count += min(mp[diff] , mp[x]);
+                mp.erase(x);
+                mp.erase(diff);
+            }
         }
-        return ans;
+        return count;
     }
 };
