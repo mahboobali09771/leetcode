9e3c3b6da6de9aa137dad02d07ddb587e7de02e9
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-06-29 17:03:37 +0530

    Time: 0 ms (100%), Space: 11.1 MB (46.55%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.cpp
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        map<int,int> mp;
-        for(auto x:arr1) mp[x]++;
+        int freq[1001] = {0};
+        for(auto x:arr1) freq[x]++;
         int j = 0;
         for(int i=0;i<arr2.size();i++){
-            while(mp[arr2[i]]--)arr1[j++] = arr2[i];
+            while(freq[arr2[i]]--)arr1[j++] = arr2[i];
         }
-        for(auto x:mp){
-            while(x.second>0){
-                arr1[j++] = x.first;
-                x.second--;
+        for(int i=0;i<=1000;i++){
+            while(freq[i] > 0){
+                arr1[j++] = i;
+                freq[i]--;
             }
         }
         return arr1;
