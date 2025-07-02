adda67ef9ab647f37c860a322ffd0ec4d20ebb13
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-07-02 06:02:19 +0530

    Time: 0 ms (100%), Space: 9.8 MB (94.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str = "";
-
-        for(int i=0;i<s.length();i++){
-                    char ch = s[i];
-        if(ch>=97 && ch<=122 || ch>=48 && ch<=57){
-            str.push_back(ch);    
-        }
-            else if(ch>=65 && ch<=90){
-            str.push_back((char)(ch+32));
+          int i=0;
+        int j=s.size()-1;
+        while(i<=j){
+            if(!isalnum(s[i])) i++;
+            else if(!isalnum(s[j])) j--;
+            else if(tolower(s[i])!=tolower(s[j])) return false;
+            else{
+                i++;
+                 j--;
             }
-            else continue;
         }
-        int n = str.length();
-        int i=0;
-        int j=n-1;
-        while(i<j){
-            if(str[i]!=str[j]) return false;
-                 i++;
-                 j--;     
-        }
-        return true;    
+        return true;
     }
 };
