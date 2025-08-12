a582d450e066e00e3f3f6cf75d19033352d710ed
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-08-12 22:18:24 +0530

    Time: 32 ms (13.83%), Space: 38.7 MB (31.16%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,73 +1,21 @@
 class Solution {
 public:
+    typedef pair<int,int> pi;
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        // S.C = O(K) & T.C = O(logn + klogk)
-        
-        int n = arr.size();
-        vector<int> ans(k);
-        if(x<arr[0]) {
-            for(int i =0;i<k;i++) {
-                ans[i] = arr[i];
-            }
-            return ans;
+        priority_queue<pi> pq; // maxheap (max size is k)
+        for(int ele : arr){
+            int distance = abs(x-ele);
+            pq.push({distance,ele});
+            if(pq.size()>k) pq.pop();
         }
-        if(x>arr[n-1]) {
-            int i = n-1, j = k-1;
-            while(j>=0) {
-                ans[j] = arr[i];
-                j--;
-                i--;
-            }
-            return ans;
+        vector<int> ans;
+        while(pq.size()>0){
+            int ele = pq.top().second;
+            ans.push_back(ele);
+            pq.pop();
         }
-        int lo = 0;
-        int hi = n-1;
-        int mid = -1;
-        int t = 0; // representing index of ans
-        bool flag  = false; // if x is present in arr or not
-        while(lo<=hi) {
-            mid = lo + (hi-lo)/2;
-            if(arr[mid] == x) {
-                flag = true;
-                ans[t] = arr[mid];
-                t++;
-                break;
-            }else if(arr[mid]>x) hi = mid-1;
-            else lo = mid + 1;
-        }
-        int lb = hi; // lb = lower bound
-        int ub = lo; // ub = upper bound
-        if(flag==true){
-            lb = mid -1;
-            ub = mid +1;
-        }
-        while(t<k && lb>=0 && ub<=n-1) {
-            int d1 = abs(x-arr[lb]);
-            int d2 = abs(x-arr[ub]);
-            if(d1<=d2) {
-                ans[t] = arr[lb];
-                lb--;
-            }else{
-                ans[t] = arr[ub];
-                ub++;
-            }
-            t++;
-        }
-        if(lb<0) {
-            while(t<k) {
-                ans[t] = arr[ub];
-                ub++;
-                t++;
-            }
-        }
-        if(ub>n-1) {
-            while(t<k) {
-                ans[t] = arr[lb];
-                lb--;
-                t++;
-            }
-        }
-        sort(ans.begin(), ans.end());
+        sort(ans.begin(),ans.end());
         return ans;
+
     }
 };
