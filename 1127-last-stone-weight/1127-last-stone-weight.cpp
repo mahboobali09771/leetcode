ea7aba1deb422c4600a86e31e173754c0cf18bbc
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-08-16 06:48:26 +0530

    Time: 0 ms (100%), Space: 9.9 MB (75.99%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.cpp
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -1,11 +1,12 @@
+/*
 class Solution {
 public:
     int lastStoneWeight(vector<int>& arr) {
         priority_queue<int> pq;
-        for(int ele : arr){
+        for(int ele : arr){ // TC->O(nlogn)
             pq.push(ele);
         }
-        while(pq.size()>1){
+        while(pq.size()>1){ // TC->O(nlogn)
             int x = pq.top();
             pq.pop();
             int y = pq.top();
@@ -13,6 +14,22 @@ public:
             if(x!=y) pq.push(x-y);
         }
         if(pq.size()==1) return pq.top();
+        else return 0;
+    }
+};
+*/
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& arr) {
+        while(arr.size()>1){ // TC->O((n-r)log(n-r)) ~ O(nlogn)
+            sort(arr.begin(),arr.end());
+            int x = arr[arr.size()-1];
+            arr.pop_back();
+            int y = arr[arr.size()-1];
+            arr.pop_back();
+            if(x!=y) arr.push_back(x-y);
+        }
+        if(arr.size()==1) return arr[0];
         else return 0;
     }
 };
