7a80b8eccda67744e23c2b9f515c2db8dc3fc753
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-08-27 06:34:13 +0530

    Time: 0 ms (100%), Space: 22.4 MB (96.58%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
         if(nums.size()<2) return nums.size();
         int k = 1;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=nums[k-1]) {
-            nums[k] = nums[i];
-                k++;
-            } 
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[k-1]){
+                nums[k] = nums[i];
+                      k++;
+            }
         }
         return k;
     }
 };
-
-// 2nd method without using pointer
-/*
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        
-        
-        vector<int> ans;
-        ans.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-           if(nums[i]!=ans[ans.size()-1]) ans.push_back(nums[i]);
-        }
-        for(int i=0;i<ans.size();i++){
-            nums[i] = ans[i];
-        }
-        return ans.size();
-        
-    }
-};
-
-
-*/
