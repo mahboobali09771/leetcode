ba9dcf8ed480455f42847ea842d2a876d04c85e7
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-09-14 05:37:51 +0530

    Time: 869 ms (5.02%), Space: 23.9 MB (55.47%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,40 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int n = nums.size();
-        int i =0; // i for travel zero
-        int j= 0; // j for travel non-zero
-        while(i<n && j<n){
-            if(nums[i] == 0 && nums[j] != 0){
-                if(i<j) swap(nums[i],nums[j]);
-                else j++;
-              }
-            
-            if(nums[i] != 0) i++;
-            if(nums[j] == 0) j++;
-            }
-    }
-};
-/*
-
-// 2nd method using extra space
-class Solution {
-public:
-    void moveZeroes(vector<int>& nums) {
-        vector<int> v;
         int n = nums.size();
-        int noz = 0;
         for(int i=0;i<n;i++){
-            if(nums[i]!=0) v.push_back(nums[i]);
-            else noz++;
+            for(int j=0;j<n-1;j++){
+                if(nums[j]==0) swap(nums[j],nums[j+1]);
+            }
         }
-        for(int i=1;i<=noz;i++) v.push_back(0);
-        
-        for(int i=0;i<n;i++){
-        nums[i] = v[i];
-        }
-        
     }
 };
-
-*/
