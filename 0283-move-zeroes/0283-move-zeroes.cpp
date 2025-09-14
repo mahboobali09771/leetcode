e509f9310a1db69b5d4aeb26161ead3b57d9b763
MOHAMMAD MAHBOOB ALI <132975341+mahboobali09771@users.noreply.github.com>
2025-09-14 05:48:38 +0530

    Time: 0 ms (100%), Space: 24 MB (10.07%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,3 +1,4 @@
+/* TC = O(n^2)
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -6,6 +7,23 @@ public:
             for(int j=0;j<n-1;j++){
                 if(nums[j]==0) swap(nums[j],nums[j+1]);
             }
+        }
+    }
+};
+*/
+class Solution { // TC = O(n)
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j= 0;
+        while(i<n && j<n){
+            if(nums[i]==0 && nums[j]!=0) {
+                if(i<j) swap(nums[i],nums[j]);
+                else j++;
+            }
+            if(nums[i]!=0) i++;
+            if(nums[j]==0) j++;
         }
     }
 };
